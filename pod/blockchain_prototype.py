b494a935ee9dc4ca46aee491b95adfe7ab97a592
huangjunqin <junqin.huang@qq.com>
2020-07-30 11:54:31 +0800

    choose another zkSNARK to implement pod

--- pod/blockchain_prototype.py
+++ pod/blockchain_prototype.py
@@ -1,7 +1,6 @@
 import hashlib
 import json
 from time import time
-from urllib.parse import urlparse
 from uuid import uuid4
 
 
@@ -10,72 +9,51 @@ class Blockchain:
         self.current_transactions = []
         self.chain = []
         self.nodes = set()
-        # Generation interval of blocks (seconds)
-        self.block_interval = 14
-        # Minimum gas needed
-        self.min_gas = 21000
-        # Update every 5 blocks
-        self.current_local_avg_gas = 1000
+
+        # Create the genesis block
+        self.new_block(previous_hash='1', proof=100)
+
         # Generate a globally unique address for this node
         self.address = str(uuid4()).replace('-', '')
-        # Create the genesis block
-        self.new_block(previous_hash='0', local_avg_gas=0, self.address)
 
     def register_node(self, instance):
         """
         Add a new node to the list of nodes
 
-        :instance: instance object of node.'
+        :param instance: instance of blockchain node.
         """
 
         self.nodes.add(instance)
 
-    def valid_block(self, block):
+    def valid_chain(self, chain):
         """
-        Determine if a given block is valid
+        Determine if a given blockchain is valid
 
-        :param block: A newest block
+        :param chain: A blockchain
         :return: True if valid, False if not
         """
 
-        last_block = self.chain[-1]
+        last_block = chain[0]
+        current_index = 1
 
-        print(f'{last_block}')
-        print(f'{block}')
-        print("\n-----------\n")
-        # Check that the index of the block is larger than local chain
-        if last_block['index'] >= block['index']:
-            return False
+        while current_index < len(chain):
+            block = chain[current_index]
+            print(f'{last_block}')
+            print(f'{block}')
+            print("\n-----------\n")
+            # Check that the hash of the block is correct
+            last_block_hash = self.hash(last_block)
+            if block['previous_hash'] != last_block_hash:
+                return False
 
-        # Check that the hash of the block is correct
-        last_block_hash = self.hash(last_block)
-        if block['previous_hash'] != last_block_hash:
-            return False
+            # Check that the Proof of Work is correct
+            if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+                return False
 
-        # Check that the Proof of Data is correct
-        if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
-            return False
-
-        last_block = block
-        current_index += 1
+            last_block = block
+            current_index += 1
 
         return True
-    
-    @staticmethod
-    def valid_proof(last_proof, proof, last_hash):
-        """
-        Validates the Proof
-
-        :param last_proof: <int> Previous Proof
-        :param proof: <int> Current Proof
-        :param last_hash: <str> The hash of the Previous Block
-        :return: <bool> True if correct, False if not.
-
-        """
-
-        guess = f'{last_proof}{proof}{last_hash}'.encode()
-        guess_hash = hashlib.sha256(guess).hexdigest()
-        return guess_hash[:4] == "0000"
 
     def resolve_conflicts(self):
         """
@@ -93,16 +71,13 @@ class Blockchain:
 
         # Grab and verify the chains from all the nodes in our network
         for node in neighbours:
-            response = requests.get(f'http://{node}/chain')
+            chain = node.full_chain
+            length = len(chain)
 
-            if response.status_code == 200:
-                length = response.json()['length']
-                chain = response.json()['chain']
-
-                # Check if the length is longer and the chain is valid
-                if length > max_length and self.valid_chain(chain):
-                    max_length = length
-                    new_chain = chain
+            # Check if the length is longer and the chain is valid
+            if length > max_length and self.valid_chain(chain):
+                max_length = length
+                new_chain = chain
 
         # Replace our chain if we discovered a new, valid chain longer than ours
         if new_chain:
@@ -111,13 +86,12 @@ class Blockchain:
 
         return False
 
-    def new_block(self, local_avg_gas, previous_hash, address):
+    def new_block(self, proof, previous_hash):
         """
         Create a new Block in the Blockchain
 
-        :param avg_proof_gas: The average proof gas given by the Proof of Data algorithm
+        :param proof: The proof given by the Proof of Work algorithm
         :param previous_hash: Hash of previous Block
-        :param address: Identifier of miner
         :return: New Block
         """
 
@@ -125,9 +99,8 @@ class Blockchain:
             'index': len(self.chain) + 1,
             'timestamp': time(),
             'transactions': self.current_transactions,
-            'local_avg_gas': local_avg_gas,
+            'proof': proof,
             'previous_hash': previous_hash or self.hash(self.chain[-1]),
-            'address': address
         }
 
         # Reset the current list of transactions
@@ -157,6 +130,10 @@ class Blockchain:
     def last_block(self):
         return self.chain[-1]
 
+    @property
+    def full_chain(self):
+        return self.chain
+
     @staticmethod
     def hash(block):
         """
@@ -169,7 +146,7 @@ class Blockchain:
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
 
-    def proof_of_data(self, last_block):
+    def proof_of_work(self, last_block):
         """
         Simple Proof of Work Algorithm:
 
@@ -188,6 +165,42 @@ class Blockchain:
             proof += 1
 
         return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof, last_hash):
+        """
+        Validates the Proof
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :param last_hash: <str> The hash of the Previous Block
+        :return: <bool> True if correct, False if not.
+
+        """
+
+        guess = f'{last_proof}{proof}{last_hash}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    def mine(self):
+        # We run the proof of work algorithm to get the next proof...
+        last_block = self.last_block
+        proof = self.proof_of_work(last_block)
+
+        # We must receive a reward for finding the proof.
+        # The sender is "0" to signify that this node has mined a new coin.
+        blockchain.new_transaction(
+            sender="0",
+            recipient=self.address,
+            amount=1,
+        )
+
+        # Forge the new Block by adding it to the chain
+        previous_hash = self.hash(last_block)
+        block = self.new_block(proof, previous_hash)
+
+        print(f'{block}')
+        print("\n-----------\n")
 
 
 if __name__ == '__main__':
