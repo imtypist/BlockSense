c11d5f94936f6ae863d88c097abe421a363215f6
huangjunqin <junqin.huang@qq.com>
2021-01-15 14:51:47 +0800

    visualize the geo-distribution of WiFi

---
+++ pod/datasets/WiFi/geo_distribution_data.m
@@ -0,0 +1,49 @@
+clear;
+clc;
+
+logs1 = csvread('logs_1.csv',1,4);
+len = length(logs1);
+workers = [];
+for i=1:len
+    workers = [workers; "Worker 1"];
+end
+
+logs2 = csvread('logs_2.csv',1,4);
+len = length(logs2);
+for i=1:len
+    workers = [workers; "Worker 2"];
+end
+
+logs3 = csvread('logs_3.csv',1,4);
+len = length(logs3);
+for i=1:len
+    workers = [workers; "Worker 3"];
+end
+
+logs4 = csvread('logs_4.csv',1,4);
+len = length(logs4);
+for i=1:len
+    workers = [workers; "Worker 4"];
+end
+
+logs5 = csvread('logs_5.csv',1,4);
+len = length(logs5);
+for i=1:len
+    workers = [workers; "Worker 5"];
+end
+
+logs6 = csvread('logs_6.csv',1,4);
+len = length(logs6);
+for i=1:len
+    workers = [workers; "Worker 6"];
+end
+
+logs = [logs1;logs2;logs3;logs4;logs5;logs6];
+
+colordata = categorical(workers);
+gb = geobubble(logs(:,2),logs(:,1),logs(:,3),'Basemap','satellite','MapLayout','maximized');
+gb.ColorData = colordata;
+gb.ColorLegendTitle = 'Workers';
+gb.SizeLegendTitle = 'Signal Strength';
+gb.FontName = 'Times New Roman';
+gb.FontSize = 14;
